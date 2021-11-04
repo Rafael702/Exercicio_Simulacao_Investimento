8e078bdd6b90af67b89a185b35a535835e2f59f8
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-04 14:42:12 -0300

    Criar método salvar investimentos na lista de investimentos.

---
+++ src/main/java/br/com/zup/SimuladorDeInvestimento/service/InvestimentoService.java
@@ -0,0 +1,19 @@
+package br.com.zup.SimuladorDeInvestimento.service;
+
+import br.com.zup.SimuladorDeInvestimento.dto.InvestimentoDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class InvestimentoService {
+    List<InvestimentoDTO> investimentos = new ArrayList<>();
+
+    public void salvarInvestimentos(InvestimentoDTO investimentoDTO){
+        investimentos.add(investimentoDTO);
+    }
+
+
+
+}
