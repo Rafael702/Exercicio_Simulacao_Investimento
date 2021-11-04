58ed3e634fd35f7fc13486e66aa1a6c6df29dae6
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-04 14:47:33 -0300

    Executar códigos.

---
+++ src/main/java/br/com/zup/SimuladorDeInvestimento/SimuladorDeInvestimentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.SimuladorDeInvestimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorDeInvestimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladorDeInvestimentoApplication.class, args);
+	}
+
+}
