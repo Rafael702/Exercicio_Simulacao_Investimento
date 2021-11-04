b9231072742fb60cb0a4af1e23a4ad3f8d767511
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-04 14:45:06 -0300

    Criar método cadastrar Investimento com o método HTTP PUT.

---
+++ src/main/java/br/com/zup/SimuladorDeInvestimento/controller/InvestimentoController.java
@@ -0,0 +1,22 @@
+package br.com.zup.SimuladorDeInvestimento.controller;
+
+import br.com.zup.SimuladorDeInvestimento.dto.InvestimentoDTO;
+import br.com.zup.SimuladorDeInvestimento.service.InvestimentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/investimentos")
+public class InvestimentoController {
+
+    @Autowired
+    private InvestimentoService investimentoService;
+
+    @PutMapping
+    public void cadastrarInvestimento(@RequestBody InvestimentoDTO investimentoDTO){
+        investimentoService.salvarInvestimentos(investimentoDTO);
+    }
+
+}
